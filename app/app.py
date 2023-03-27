99818355d1256d5bbb5f28ec1b8fc128a38d1889
Daniel Trier <daniel.trier@code.berlin>
2023-03-27 19:39:23 +0200

    refactored with factory pattern

--- app/app.py
+++ app/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, redirect, url_for, render_template, send_file
 from random import randint
 from . import simple_pages
-app = Flask(__name__)
-app.config.from_object('app.config')
+
+def create_app():
+  app = Flask(__name__)
+  app.config.from_object('app.config')
+  register_blueprints(app)
+
+  return app
 
 
-
-app.register_blueprint(simple_pages.routes.blueprint)
+def register_blueprints(app: Flask):
+  app.register_blueprint(simple_pages.routes.blueprint)
 
 
 
